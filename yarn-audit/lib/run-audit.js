a9f9d534b70d855a837e2d6cc959d4fa2a48f89a
kilpatty <seanpkilgarriff@gmail.com>
2019-12-10 09:06:18 -0600

    yarn-audit: fix access to json

--- yarn-audit/lib/run-audit.js
+++ yarn-audit/lib/run-audit.js
@@ -1,20 +1,24 @@
 module.exports = async tools => {
-  const result = await tools.runInWorkspace(
-    'yarn',
-    ['audit', '--json'],
-    { reject: false }
-  )
+  const result = await tools.runInWorkspace("yarn", ["audit", "--json"], {
+    reject: false
+  });
 
   try {
     // Try to parse the result as actual JSON
-    const json = JSON.parse(result.stdout)
-    const vulns = json.metadata.vulnerabilities
+    const summary = JSON.parse(result.stdout.pop());
+    const json = JSON.parse(result.stdout);
+    console.log(json);
+    // const vulns = json.metadata.vulnerabilities
+    const vulns = summary.data.vulnerabilities;
 
     // Get the total count of vulnerabilities
-    const keys = Object.keys(vulns)
-    const numVulnerabilities = keys.reduce((p, c) => { p += vulns[c]; return p }, 0)
-    return { vulnerabilities: vulns, numVulnerabilities }
+    const keys = Object.keys(vulns);
+    const numVulnerabilities = keys.reduce((p, c) => {
+      p += vulns[c];
+      return p;
+    }, 0);
+    return { vulnerabilities: vulns, numVulnerabilities };
   } catch (err) {
-    throw err
+    throw err;
   }
-}
+};
