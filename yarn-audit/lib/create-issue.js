34a38d0270a1d275b074822414e2a98e15f634c0
kilpatty <seanpkilgarriff@gmail.com>
2019-12-11 16:49:05 -0600

    yarn-audit: attempt new context format

--- yarn-audit/lib/create-issue.js
+++ yarn-audit/lib/create-issue.js
@@ -2,13 +2,15 @@ const github = require("@actions/github");
 const createBody = require("./create-body");
 
 let createIssue = async ({ toolkit, vulnerabilities, numVulnerabilities }) => {
-  const context = toolkit.context;
+  // const context = toolkit.context;
+  const context = github.context;
 
-  return await toolkit.issues.create({
-    ...context.repo,
-    title: "Yarn.lock Vulernabilities",
-    body: createBody(vulnerabilities, numVulnerabilities)
-  });
+  return await toolkit.issues.create(
+    context.repo({
+      title: "Yarn.lock Vulernabilities",
+      body: createBody(vulnerabilities, numVulnerabilities)
+    })
+  );
 };
 
 module.exports = createIssue;
