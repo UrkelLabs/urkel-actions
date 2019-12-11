3bbfd5653c560bfc659a3ac58441d361b2d453f2
kilpatty <seanpkilgarriff@gmail.com>
2019-12-11 16:30:26 -0600

    yarn-audit: toolkit fix and fix formatting

--- yarn-audit/lib/create-issue.js
+++ yarn-audit/lib/create-issue.js
@@ -1,15 +1,14 @@
 const github = require("@actions/github");
 const createBody = require("./create-body");
 
-let createIssue = async({toolkit, vulnerabilities, numVulnerabilities}) => {
-    const context = github.context;
+let createIssue = async ({ toolkit, vulnerabilities, numVulnerabilities }) => {
+  const context = github.context;
 
-        return await octokit.issues.create({
-  ...context.repo,
-  title: 'Yarn.lock Vulernabilities',
-  body: createBody(vulnerabilities, numVulnerabilities)
-});
-
-}
+  return await toolkit.issues.create({
+    ...context.repo,
+    title: "Yarn.lock Vulernabilities",
+    body: createBody(vulnerabilities, numVulnerabilities)
+  });
+};
 
 module.exports = createIssue;
