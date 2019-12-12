21cc2bac1843f29505c690aa8a080729891f35d2
kilpatty <seanpkilgarriff@gmail.com>
2019-12-11 19:16:07 -0600

    octodns: split into functions, add deploy functionality

--- octodns/index.js
+++ octodns/index.js
@@ -2,43 +2,61 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 const github = require("@actions/github");
 
-async function run() {
+async function dryRun() {
   let dryRunOutput = "";
-  try {
-    const options = {};
-    options.listeners = {
-      stdout: data => {
-        dryRunOutput += data.toString();
-      }
-    };
-
-    await exec.exec(
-      "octodns-sync",
-      ["--config-file", "./config/production.yml"],
-      options
-    );
-
-    const result = dryRunOutput
-      .split(
-        "********************************************************************************"
-      )
-      .pop();
-
-    const token = core.getInput("token");
-
-    const context = github.context;
-    if (context.payload.pull_request == null) {
-      core.setFailed("No pull request found.");
-      return;
+  const options = {};
+  options.listeners = {
+    stdout: data => {
+      dryRunOutput += data.toString();
     }
-    const pull_request_number = context.payload.pull_request.number;
+  };
 
-    const octokit = new github.GitHub(token);
-    const new_comment = octokit.issues.createComment({
-      ...context.repo,
-      issue_number: pull_request_number,
-      body: result
-    });
+  await exec.exec(
+    "octodns-sync",
+    ["--config-file", "./config/production.yml"],
+    options
+  );
+
+  const result = dryRunOutput
+    .split(
+      "********************************************************************************"
+    )
+    .pop();
+
+  const token = core.getInput("token");
+
+  const context = github.context;
+  if (context.payload.pull_request == null) {
+    core.setFailed("No pull request found.");
+    return;
+  }
+  const pull_request_number = context.payload.pull_request.number;
+
+  const octokit = new github.GitHub(token);
+  const new_comment = octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request_number,
+    body: result
+  });
+}
+
+async function deploy() {
+  await exec.exec("octodns-sync", [
+    "--config-file",
+    "./config/production.yml",
+    "--doit"
+  ]);
+}
+
+async function run() {
+  try {
+    const deploy = core.getInput("deploy");
+
+    if (deploy === true) {
+      await deploy();
+    } else {
+      await dryRun();
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
